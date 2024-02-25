88da477d9a7967536afc9ba7f80b25d70711c032
Douglas Liebl <douglasliebl@outlook.com>
2024-02-25 13:02:00 -0300

    Add: handling allowed files extension

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,6 +1,16 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+import StandardError from '../exceptions/StandardError';
+
+const upload = multer(multerConfig).single('image');
+
 class ImageController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) return res.status(400).json(new StandardError(400, req.method, req.path, err.errors.map((e) => e.message)));
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/image.js
+++ src/routes/image.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import ImageController from '../controllers/ImageController';
-
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/image', upload.single('image'), ImageController.store);
+router.post('/image', ImageController.store);
 
 export default router;
